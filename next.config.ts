9fa48128ae787ab1a209b28bc85b9cfab61507f8
mihir-n25 <mihirnebani@gmail.com>
2025-09-15 10:39:10 +0530

    depoy issue fixed : nextconfig

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true, // TS might complain
-  } as any,       // ← cast to any
   images: {
     domains: ["i.pinimg.com"],
   },
